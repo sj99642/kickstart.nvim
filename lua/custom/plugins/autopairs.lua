7da03f7c45619abf7a34efe90a89d17d8d478b3b
Stephen Jones <stephenj99642@gmail.com>
2023-11-22 21:23:06 +0000

    Add autopairs and filetree plugins

---
+++ lua/custom/plugins/autopairs.lua
@@ -0,0 +1,15 @@
+-- Added this based on the instructions in kickstarter's readme
+return {
+    "windwp/nvim-autopairs",
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+        require('nvim-autopairs').setup {}
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local cmp = require('cmp')
+        cmp.event:on(
+            'confirm_done',
+            cmp_autopairs.on_confirm_done()
+        )
+    end,
+}
+
